ba69f5e38ea9c0c665f23982f94adb248547ea47
lee <mbc09111@naver.com>
2024-03-29 15:26:44 +0900

    쓸데없는거 제거

--- ljy_app/FeedFood/lib/const/colors.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-
-const PRIMARY_COLOR = Colors.black;
-final LIGHT_GREY_COLOR = Colors.grey[200]!;
-final DARK_GREY_COLOR = Colors.grey[600]!;
-final TEXT_FIELD_FILL_COLOR = Colors.grey[300]!;
-final LIGHT_RED_COLOR = Colors.red[300]!;

a5ab31804fa8e34e5e3ee7080cb932ebfe36f835
seunghanlee <seunghan713@sunmoon.ac.kr>
2024-04-13 10:00:07 +0900

    유튜브 채널, 채널별 영상, 영상별 댓글 기능 추가

--- backend/ysmProject3/src/main/java/org/mysite/ysmproject3/youtube/YouTubeController.java
+++ backend/ysmProject3/src/main/java/org/mysite/ysmproject3/youtube/YouTubeController.java
@@ -1,6 +1,10 @@
 package org.mysite.ysmproject3.youtube;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import com.google.api.services.youtube.YouTube;
+import lombok.RequiredArgsConstructor;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
@@ -15,26 +19,26 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.util.ArrayList;
+import java.util.List;
+
 
 @RestController
-
+@RequiredArgsConstructor
 public class YouTubeController {
-
     //임시 영상 ID
     private static final String VIDEO_ID = "1ifSAFCGiX8";
 
-    @Autowired
     private final OAuth2AuthorizedClientService authorizedClientService;
-
-    public YouTubeController(OAuth2AuthorizedClientService authorizedClientService) {
-        this.authorizedClientService = authorizedClientService;
-    }
+    private final YoutubeService youtubeService;
 
     //유튜브 채널 목록
     //http://localhost:8080/youtube/channel
     @GetMapping("/youtube/channel")
     @ResponseBody
-    public String callYouTubeApi(Authentication authentication) {
+    public List<String> callYouTubeApi(Authentication authentication) throws IOException, GeneralSecurityException {
         OAuth2AuthenticationToken oauthToken = (OAuth2AuthenticationToken) authentication;
         OAuth2AuthorizedClient client = authorizedClientService
                 .loadAuthorizedClient(
@@ -46,28 +50,39 @@ public class YouTubeController {
         }
 
         String accessToken = client.getAccessToken().getTokenValue();
-        // 이제 accessToken을 사용하여 YouTube Data API 호출 가능
-        // 메서드 내부에서
-        RestTemplate restTemplate = new RestTemplate();
-        HttpHeaders headers = new HttpHeaders();
-        headers.setBearerAuth(accessToken);
-        HttpEntity<String> entity = new HttpEntity<>("parameters", headers);
 
-        ResponseEntity<String> response = restTemplate.exchange(
-                "https://www.googleapis.com/youtube/v3/subscriptions?part=snippet&mine=true&maxResults=25", // YouTube Data API 요청 URL 예시
-                HttpMethod.GET,
-                entity,
-                String.class);
-
-        return response.getBody(); // API 응답 내용
-//        return "test";
+        return youtubeService.getSubscriptions(accessToken); // API 응답 내용
     }
+
+    //채널 페이지별 영상 가져오기
+    //http://localhost:8080/youtube/channelvideos?channelId=UClWg5n2YST6_697s-zQSMnA&nextPageToken=CA8QAA
+    @GetMapping("/youtube/channelvideos")
+    @ResponseBody
+    public String getChannelList(Authentication authentication, @RequestParam String channelId, @RequestParam String nextPageToken) throws IOException, GeneralSecurityException {
+        OAuth2AuthenticationToken oauthToken = (OAuth2AuthenticationToken) authentication;
+        OAuth2AuthorizedClient client = authorizedClientService
+                .loadAuthorizedClient(
+                        oauthToken.getAuthorizedClientRegistrationId(),
+                        oauthToken.getName());
+        if (client == null) {
+            // 적절한 예외 처리 또는 로그 출력
+            throw new IllegalStateException("OAuth2AuthorizedClient not found. Are you logged in?");
+        }
+
+        String accessToken = client.getAccessToken().getTokenValue();
+
+        return youtubeService.getPageVideos(accessToken, channelId, nextPageToken);
+    }
+
+
+
+
 
     //유튜브 댓글 가져오기
     //http://localhost:8080/youtube/comments?videoId=1ifSAFCGiX8
     @GetMapping("/youtube/comments")
     @ResponseBody
-    public String getVideoComments(Authentication authentication, @RequestParam String videoId) {
+    public String getVideoComments(Authentication authentication, @RequestParam String videoId) throws IOException, GeneralSecurityException {
         OAuth2AuthenticationToken oauthToken = (OAuth2AuthenticationToken) authentication;
         OAuth2AuthorizedClient client = authorizedClientService
                 .loadAuthorizedClient(
@@ -80,24 +95,187 @@ public class YouTubeController {
 
         String accessToken = client.getAccessToken().getTokenValue();
 
-        System.out.println(accessToken);
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        headers.setBearerAuth(accessToken);
+        HttpEntity<?> entity = new HttpEntity<>(headers);
+
+        return youtubeService.getComments(accessToken, videoId);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //유튜브 자막 추출
+    //http://localhost:8080/youtube/captionlist?videoId=1ifSAFCGiX8
+    @GetMapping("/youtube/captiondownload")
+    @ResponseBody
+    public String getYouTubeCaptionDownloader(Authentication authentication,@RequestParam String videoId) throws IOException, GeneralSecurityException {
+        OAuth2AuthenticationToken oauthToken = (OAuth2AuthenticationToken) authentication;
+        OAuth2AuthorizedClient client = authorizedClientService
+                .loadAuthorizedClient(
+                        oauthToken.getAuthorizedClientRegistrationId(),
+                        oauthToken.getName());
+
+        if (client == null) {
+            throw new IllegalStateException("OAuth2AuthorizedClient not found. Are you logged in?");
+        }
+
+        String accessToken = client.getAccessToken().getTokenValue();
 
         RestTemplate restTemplate = new RestTemplate();
         HttpHeaders headers = new HttpHeaders();
         headers.setBearerAuth(accessToken);
         HttpEntity<?> entity = new HttpEntity<>(headers);
 
-        String url = String.format("https://www.googleapis.com/youtube/v3/commentThreads?part=snippet&videoId=%s&textFormat=plainText&maxResults=20", videoId);
+        String url1 = String.format("https://www.googleapis.com/youtube/v3/captions?part=snippet&videoId=%s", videoId);
 
-        ResponseEntity<String> response = restTemplate.exchange(
-                url,
+        ResponseEntity<String> response1 = restTemplate.exchange(
+                url1,
                 HttpMethod.GET,
                 entity,
                 String.class);
 
-        return response.getBody();
+        JSONObject jsonResponse = new JSONObject(response1.getBody());
+        JSONArray items = jsonResponse.getJSONArray("items");
+
+        if (items != null && items.length() > 0) {
+            JSONObject firstItem = items.getJSONObject(0);
+            String id = firstItem.getString("id");
+
+            String url2 = String.format("https://www.googleapis.com/youtube/v3/captions/%s?tfmt=vtt", id);
+
+            System.out.println(url2);
+            ResponseEntity<String> response2 = restTemplate.exchange(
+                    url2,
+                    HttpMethod.GET,
+                    entity,
+                    String.class);
+
+            return response2.getBody();  // Return only the ID of the first caption
+        } else {
+            return "No captions found for this video.";
+        }
+
     }
 
+    //http://localhost:8080/youtube/captiondown?id=AUieDabfxc8sHdmoHb-04rA0aGWfOYNqh_qmv117sTjQmj0WyGQ
+    @GetMapping("/youtube/captiondown")
+    @ResponseBody
+    public String getcaptiondown(Authentication authentication,@RequestParam String id) {
+        OAuth2AuthenticationToken oauthToken = (OAuth2AuthenticationToken) authentication;
+        OAuth2AuthorizedClient client = authorizedClientService
+                .loadAuthorizedClient(
+                        oauthToken.getAuthorizedClientRegistrationId(),
+                        oauthToken.getName());
+
+        if (client == null) {
+            throw new IllegalStateException("OAuth2AuthorizedClient not found. Are you logged in?");
+        }
+
+        String accessToken = client.getAccessToken().getTokenValue();
+
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        headers.setBearerAuth(accessToken);
+        HttpEntity<?> entity = new HttpEntity<>(headers);
+
+        String url2 = String.format("https://www.googleapis.com/youtube/v3/captions/%s?tfmt=vtt", id);
+
+        System.out.println(url2);
+        ResponseEntity<String> response2 = restTemplate.exchange(
+                url2,
+                HttpMethod.GET,
+                entity,
+                String.class);
+
+        return response2.getBody();
+
+    }
+
+    //http://localhost:8080/youtube/captionlist?videoId=1ifSAFCGiX8
+    @GetMapping("/youtube/captionlist")
+    @ResponseBody
+    public String getcaptionlist(Authentication authentication,@RequestParam String videoId) throws IOException, GeneralSecurityException {
+        OAuth2AuthenticationToken oauthToken = (OAuth2AuthenticationToken) authentication;
+        OAuth2AuthorizedClient client = authorizedClientService
+                .loadAuthorizedClient(
+                        oauthToken.getAuthorizedClientRegistrationId(),
+                        oauthToken.getName());
+
+        if (client == null) {
+            throw new IllegalStateException("OAuth2AuthorizedClient not found. Are you logged in?");
+        }
+
+        String accessToken = client.getAccessToken().getTokenValue();
+
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        headers.setBearerAuth(accessToken);
+        HttpEntity<?> entity = new HttpEntity<>(headers);
+
+        String url1 = String.format("https://www.googleapis.com/youtube/v3/captions?part=snippet&videoId=%s", videoId);
+
+        ResponseEntity<String> response1 = restTemplate.exchange(
+                url1,
+                HttpMethod.GET,
+                entity,
+                String.class);
+
+        JSONObject jsonResponse = new JSONObject(response1.getBody());
+        JSONArray items = jsonResponse.getJSONArray("items");
+
+        if (items != null && items.length() > 0) {
+            JSONObject firstItem = items.getJSONObject(0);
+            String id = firstItem.getString("id");
+
+            return id;  // Return only the ID of the first caption
+        } else {
+            return "No captions found for this video.";
+        }
+
+    }
 
 
 
--- backend/ysmProject3/src/main/java/org/mysite/ysmproject3/youtube/YoutubeService.java
+++ backend/ysmProject3/src/main/java/org/mysite/ysmproject3/youtube/YoutubeService.java
@@ -1,2 +1,99 @@
-package org.mysite.ysmproject3.youtube;public class YoutubeService {
+package org.mysite.ysmproject3.youtube;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class YoutubeService {
+
+    //구독아이디 리스트로 반환
+    public List<String> getSubscriptions(String accessToken) throws IOException, GeneralSecurityException {
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        headers.setBearerAuth(accessToken);
+        HttpEntity<String> entity = new HttpEntity<>("parameters", headers);
+
+        ResponseEntity<String> response = restTemplate.exchange(
+                "https://www.googleapis.com/youtube/v3/subscriptions?part=snippet&mine=true&maxResults=5",
+                HttpMethod.GET,
+                entity,
+                String.class);
+
+        // JSON 응답 파싱
+        JSONObject jsonResponse = new JSONObject(response.getBody());
+        JSONArray items = jsonResponse.getJSONArray("items");
+        List<String> subscriptionsId = new ArrayList<>();
+
+        for (int i = 0; i < items.length(); i++) {
+            JSONObject item = items.getJSONObject(i);
+            JSONObject snippet = item.getJSONObject("snippet");
+            JSONObject resourceId = snippet.getJSONObject("resourceId");
+            subscriptionsId.add(resourceId.getString("channelId"));  // id 값만 리스트에 추가
+        }
+        return subscriptionsId;
+    }
+
+    //채널 동영상 출력
+    public String getPageVideos(String accessToken, String channelId, String nextPageToken) {
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        headers.setBearerAuth(accessToken);
+        HttpEntity<String> entity = new HttpEntity<>("parameters", headers);
+
+        if (nextPageToken != "") {
+
+            String url = String.format("https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&channelId=%s&pageToken=%s", channelId, nextPageToken);
+
+            ResponseEntity<String> response = restTemplate.exchange(
+                    url,
+                    HttpMethod.GET,
+                    entity,
+                    String.class);
+
+            return response.getBody();
+        } else {
+            String url = String.format("https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&channelId=%s", channelId);
+
+            ResponseEntity<String> response = restTemplate.exchange(
+                    url,
+                    HttpMethod.GET,
+                    entity,
+                    String.class);
+
+            return response.getBody();
+        }
+
+    }
+
+    //유튜브 댓글 가져오기
+    //100개만 출력
+    public String getComments(String accessToken, String videoId) {
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        headers.setBearerAuth(accessToken);
+        HttpEntity<?> entity = new HttpEntity<>(headers);
+
+        String url = String.format("https://www.googleapis.com/youtube/v3/commentThreads?part=snippet&videoId=%s&textFormat=plainText&maxResults=100", videoId);
+
+        ResponseEntity<String> response = restTemplate.exchange(
+                url,
+                HttpMethod.GET,
+                entity,
+                String.class);
+
+        return response.getBody();
+    }
+    
+
 }
